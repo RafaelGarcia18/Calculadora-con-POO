74ca5df93245c5c9a6fabe8ef9f9495b7344d0a4
Rafael Garcia <ge17001@ues.edu.sv>
2022-03-18 09:49:17 -0600

    Suma realizada y funcionando

---
+++ src/Calculadora.java
@@ -0,0 +1,12 @@
+public class Calculadora {
+
+    public Double sumar(Double num1, Double num2) {
+        if (num1 == null || num2 == null) {
+
+            return null;
+        } else {
+            return num1 + num2;
+        }
+    }
+
+}
