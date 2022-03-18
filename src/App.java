f47ea5a1f881f7bff59a314a197d7cd096839f54
Rafael Garcia <ge17001@ues.edu.sv>
2022-03-18 10:17:00 -0600

    Suma realizada

--- src/App.java
+++ src/App.java
@@ -1,6 +1,15 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Bienvenido a nuestra calculadora");
-        
+        Calculadora cal = new Calculadora();
+
+        Double primero = Double.parseDouble(JOptionPane.showInputDialog("Insserte el primer numero"));
+        Double segundo = Double.parseDouble(JOptionPane.showInputDialog("Insserte el segundo numero"));
+        Double resultado = cal.sumar(primero, segundo);
+
+        JOptionPane.showMessageDialog(null,
+                "El resultado de la suma de " + primero + " + " + segundo + " = " + resultado);
     }
 }
