0a6badcd6655b9aa090bf4b58b0c835791223f58
Rafael Garcia <ge17001@ues.edu.sv>
2022-03-18 09:51:56 -0600

    Resta realizada y funcionando

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -9,4 +9,12 @@ public class Calculadora {
         }
     }
 
+    public Double restar(Double num1, Double num2) {
+        if (num1 == null || num2 == null) {
+            return null;
+        } else {
+            return num1 - num2;
+        }
+    }
+
 }
